368525f12c6adfb2ddcdf71f7b6282ae81adfcf9
sembori <sembori@yandex.ru>
2022-12-20 01:52:51 +0300

    Четное ли число

---
+++ EvenNumber/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+
+WriteLine("Давайте проверим. является ли число четным?");
+Write("Введите число: ");
+int a = int.Parse(ReadLine()!);
+
+if (a%2 == 0){
+    WriteLine($"Число {a} - четное!");
+}else{
+    WriteLine($"Увы, число {a} - нечетное. Стыдно не знать!");
+}
