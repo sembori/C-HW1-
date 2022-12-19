ca5f5333e299f5b38abd00d065df1cd23da7df29
sembori <sembori@yandex.ru>
2022-12-20 01:15:58 +0300

    Наибольшее из трех чисел

---
+++ three numders/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+
+WriteLine("Введите три чиcлa, наибольшее из которых вы хотите найти!");
+Write("Первое число: ");
+int a = int.Parse(ReadLine()!);
+Write("Так, теперь второе число:");
+int b = int.Parse(ReadLine()!);
+Write("И наконец - число номер три:");
+int c = int.Parse(ReadLine()!);
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+
+    WriteLine($"Наибольшее число {max}");
