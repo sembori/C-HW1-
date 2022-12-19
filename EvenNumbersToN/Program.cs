bb4c4f54a591b42b7ffcd51e024da9952f47e354
sembori <sembori@yandex.ru>
2022-12-20 02:16:57 +0300

    Четное число до N

---
+++ EvenNumbersToN/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+
+Write("Введите число: ");
+int N = int.Parse(ReadLine()!);
+int n = 1;
+if (N <= 1)
+{
+    WriteLine("Введите большее число");
+}
+else
+{
+    while (n <= N)
+    {
+        if (n % 2 == 0)
+        {
+            Write($"{n} ");
+            n++;
+        } 
+        else{
+            n++;
+        }
+    }
+}
