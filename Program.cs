60e5c54ec10c07a5de7eb329f3e18b97badccc8e
sembori <sembori@yandex.ru>
2022-12-20 00:43:51 +0300

    Наибольшее из двух ввуденных чисел

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+
+WriteLine("Введите два чиcлa, которые вы хотите сравнить!");
+Write("Первое число: ");
+int a = int.Parse(ReadLine()!);
+Write("Второе число:");
+int b = int.Parse(ReadLine()!);
+
+if (a > b)
+{
+    WriteLine($"Число {a} больше");
+}
+else if (b > a)
+{
+    WriteLine($"Число {b} больше");
+}
+else
+{
+    WriteLine("Числа равны");
+
+}
